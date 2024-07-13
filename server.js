6056c5344a1e890c26db9cbf82c5a51179edca8e
farid <faride13578642@gmail.com>
2024-07-13 23:49:33 +0330

    add some logs

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const seen=[];
 async function check(){
+  
   try{
     const data=await axios.get("https://fp.snapptrip.com/bus-listing-go/v2/availability/11320000/to/21310000/on/2024-07-14?filter=true")
     await Promise.all(data.data.solutions.map(async (s)=>{
-      if(seen.find(se=>se===(s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name).toString())){
-        return
-      }
-      text=`
+
+      const text=`
 ${(new Date()).toLocaleTimeString()}
       ${s.originTerminal?.name} -> ${s.destinationTerminal?.name}
       ${s.departureTime}
 https://pwa.snapptrip.com/bus/search?origin=11320000&originCity=%D8%AA%D9%87%D8%B1%D8%A7%D9%86&dest=21310000&destCity=%D8%A7%D8%B5%D9%81%D9%87%D8%A7%D9%86&date=2024-07-14&abroad=false
       `
+      const bilitsDiv=document.getElementById("bilits");
+      bilitsDiv.innerHTML=""
+      const bilitP=document.createElement("p");
+      bilitP.innerHTML=text;
+      bilitsDiv.appendChild(bilitP)
+
+      if(seen.find(se=>se===(s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name).toString())){
+        return
+      }
       console.log(text);
       await axios.post(`https://api.telegram.org/bot1143862654:AAGhppoql_wjQzJ-SXCDS1ZA98lFLgbbvBQ/sendMessage?`,{chat_id:58235922,text})
 
@@ -21,6 +29,8 @@ https://pwa.snapptrip.com/bus/search?origin=11320000&originCity=%D8%AA%D9%87%D8%
   }
   catch(err){
     console.log(err);
+    const errorDiv=document.getElementById("error");
+    errorDiv.innerHTML=err;
   }
 }
 
