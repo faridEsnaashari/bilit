3715fefb1b3a1a8d7032c58ebe6de141fbf848fa
farid <F.Esnaashari@drnext.ir>
2024-07-14 10:32:52 +0330

    test

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ async function check(){
   
   try{
     const data=await axios.get("https://fp.snapptrip.com/bus-listing-go/v2/availability/11320000/to/21310000/on/2024-07-14?filter=true")
-    await Promise.all(data.data.solutions.map(async (s)=>{
+    data.data.solutions.map(async (s)=>{
 
       const text=`
 ${(new Date()).toLocaleTimeString()}
       ${s.originTerminal?.name} -> ${s.destinationTerminal?.name}
       ${s.departureTime}
+      یکشنبه
 https://pwa.snapptrip.com/bus/search?origin=11320000&originCity=%D8%AA%D9%87%D8%B1%D8%A7%D9%86&dest=21310000&destCity=%D8%A7%D8%B5%D9%81%D9%87%D8%A7%D9%86&date=2024-07-14&abroad=false
       `
       const bilitsDiv=document.getElementById("bilits");
@@ -17,15 +18,39 @@ https://pwa.snapptrip.com/bus/search?origin=11320000&originCity=%D8%AA%D9%87%D8%
       bilitP.innerHTML=text;
       bilitsDiv.appendChild(bilitP)
 
-      if(seen.find(se=>se===(s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name).toString())){
+      if(seen.find(se=>se===(s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name+"یکشنبه").toString())){
         return
       }
-      console.log(text);
       await axios.post(`https://api.telegram.org/bot1143862654:AAGhppoql_wjQzJ-SXCDS1ZA98lFLgbbvBQ/sendMessage?`,{chat_id:58235922,text})
 
-      seen.push((s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name).toString())
+      seen.push((s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name+"یکشنبه").toString())
 
-    }))
+    })
+
+    const data2=await axios.get("https://fp.snapptrip.com/bus-listing-go/v2/availability/11320000/to/21310000/on/2024-07-15?filter=true")
+    data2.data.solutions.map(async (s)=>{
+
+      const text=`
+${(new Date()).toLocaleTimeString()}
+      ${s.originTerminal?.name} -> ${s.destinationTerminal?.name}
+      ${s.departureTime}
+      دوشنبه
+https://pwa.snapptrip.com/bus/search?origin=11320000&originCity=%D8%AA%D9%87%D8%B1%D8%A7%D9%86&dest=21310000&destCity=%D8%A7%D8%B5%D9%81%D9%87%D8%A7%D9%86&date=2024-07-15&abroad=false
+      `
+      const bilitsDiv=document.getElementById("bilits2");
+      bilitsDiv.innerHTML=""
+      const bilitP=document.createElement("p");
+      bilitP.innerHTML=text;
+      bilitsDiv.appendChild(bilitP)
+
+      if(seen.find(se=>se===(s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name+"دوشنبه").toString())){
+        return
+      }
+      await axios.post(`https://api.telegram.org/bot1143862654:AAGhppoql_wjQzJ-SXCDS1ZA98lFLgbbvBQ/sendMessage?`,{chat_id:58235922,text})
+
+      seen.push((s.departureTime+s.provider+s.originTerminal.name+s.destinationTerminal.name+"دوشنبه").toString())
+
+    })
   }
   catch(err){
     console.log(err);
