a4c8ddd131edfc6cf50ee4e41a2b3512154ebb32
farid <faride13578642@gmail.com>
2024-08-06 20:49:22 +0330

    test

--- services/loggers/telegram/mappers/telegram.mapper.js
+++ services/loggers/telegram/mappers/telegram.mapper.js
@@ -1,1 +1,17 @@
-function mapper(buses) {}
+function mapper(data) {
+  return data.busses
+    .map(({ data, id }) =>
+      data.map(
+        (b) => `
+      ${new Date().toLocaleTimeString()}
+      ${b.originTerminal} -> ${b.destTerminal}
+      ${b.time}
+      ${b.date}
+      ${id}
+      `
+      )
+    )
+    .flat();
+}
+
+module.exports = mapper;
