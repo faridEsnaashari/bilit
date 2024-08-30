7b35e578af4302a29ef772c57a198851ec9a7b31
farid <F.Esnaashari@drnext.ir>
2024-08-30 17:55:40 +0330

    chore(server): make telegram and console logs seprated

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const { destCity, originCity, interval, date } = require("./config");
-const log = require("./services/loggers/logger.service");
+const {
+  logger,
+  consoleLogger,
+  telegramLogger,
+  configs,
+} = require("./services/loggers/logger.service");
 const { fetchBuses } = require("./services/providers/fetch-buses.service");
 
 setInterval(async () => {
   const today = await fetchBuses(originCity, destCity, date);
 
-  log(today);
+  const allLogs = logger([consoleLogger]);
+  const onlyUniqueBusses = logger([telegramLogger], {
+    filters: [configs.filters.onlyNotLoggedBusses],
+  });
+
+  allLogs(today);
+  onlyUniqueBusses(today);
 }, interval);
